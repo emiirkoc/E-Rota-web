daea554a5259da3cdd924f897c040e6deacb9fbc
emiirkoc <kocemir6634@gmail.com>
2026-06-07 02:45:30 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,112 @@
-alert("Giriş başarılı 🚗⚡");
+import { auth } from "./firebase.js";
 
-document.querySelector("main").innerHTML = `
-<div class="card">
-    <h2>Hoş Geldin 👋</h2>
-    <p>${email}</p>
-    <br>
-    <button onclick="location.reload()">
-        Çıkış Yap
-    </button>
-</div>
-`;
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+const loginBtn = document.getElementById("loginBtn");
+const registerBtn = document.getElementById("registerBtn");
+
+// Kayıt Ol
+registerBtn.addEventListener("click", async () => {
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (!email || !password) {
+        alert("E-posta ve şifre giriniz.");
+        return;
+    }
+
+    try {
+
+        await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        alert("Kayıt başarılı 🎉");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert(
+            "Hata: " + error.message
+        );
+
+    }
+
+});
+
+// Giriş Yap
+loginBtn.addEventListener("click", async () => {
+
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (!email || !password) {
+        alert("E-posta ve şifre giriniz.");
+        return;
+    }
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        alert("Giriş başarılı 🚗⚡");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert(
+            "Giriş başarısız: " + error.message
+        );
+
+    }
+
+});
+
+// Kullanıcı giriş yaptıysa
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+
+        document.querySelector("main").innerHTML = `
+            <section class="card">
+                <h2>👋 Hoş Geldin</h2>
+                <p>${user.email}</p>
+
+                <br>
+
+                <button id="logoutBtn">
+                    Çıkış Yap
+                </button>
+            </section>
+        `;
+
+        document
+        .getElementById("logoutBtn")
+        .addEventListener("click", async () => {
+
+            await signOut(auth);
+
+            location.reload();
+
+        });
+
+    }
+
+});
