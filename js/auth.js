77c1043456dcdccaabf628fe76acffeb04c7f25e
emiirkoc <kocemir6634@gmail.com>
2026-06-07 02:42:30 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,37 +1,12 @@
-loginBtn.addEventListener("click", async () => {
+alert("Giriş başarılı 🚗⚡");
 
-    alert("GİRİŞ BUTONU ÇALIŞTI");
-
-    const email =
-    document.getElementById("email").value;
-
-    const password =
-    document.getElementById("password").value;
-
-    console.log(email);
-    console.log(password);
-
-    try {
-
-        const userCredential =
-        await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-
-        console.log(userCredential);
-
-        alert("Giriş başarılı 🚗⚡");
-
-    } catch(error){
-
-        console.error(error);
-
-        alert(
-            error.code + "\n" + error.message
-        );
-
-    }
-
-});
+document.querySelector("main").innerHTML = `
+<div class="card">
+    <h2>Hoş Geldin 👋</h2>
+    <p>${email}</p>
+    <br>
+    <button onclick="location.reload()">
+        Çıkış Yap
+    </button>
+</div>
+`;
