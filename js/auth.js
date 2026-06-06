3964e3bcb6c9ebcea79784c8f9e87d1822cc7027
emiirkoc <kocemir6634@gmail.com>
2026-06-07 02:38:44 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,74 +1,9 @@
-console.log("AUTH ÇALIŞTI");
+await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+);
 
-import { auth } from "./firebase.js";
+alert("Giriş başarılı 🚗⚡");
 
-import {
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-const loginBtn = document.getElementById("loginBtn");
-const registerBtn = document.getElementById("registerBtn");
-
-registerBtn.addEventListener("click", async () => {
-
-    console.log("KAYIT TIKLANDI");
-
-    const email =
-    document.getElementById("email").value;
-
-    const password =
-    document.getElementById("password").value;
-
-    try {
-
-        const result =
-        await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-
-        console.log(result);
-
-        alert("Kayıt başarılı 🎉");
-
-    } catch(error){
-
-        console.error(error);
-
-        alert(error.code);
-
-    }
-
-});
-
-loginBtn.addEventListener("click", async () => {
-
-    console.log("GİRİŞ TIKLANDI");
-
-    const email =
-    document.getElementById("email").value;
-
-    const password =
-    document.getElementById("password").value;
-
-    try {
-
-        await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-        );
-
-        alert("Giriş başarılı 🚗⚡");
-
-    } catch(error){
-
-        console.error(error);
-
-        alert(error.code);
-
-    }
-
-});
+window.location.reload();
