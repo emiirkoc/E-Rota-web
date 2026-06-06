1bfc044872f0e61c2b223f3f9be75ede9dfec94d
emiirkoc <kocemir6634@gmail.com>
2026-06-07 02:40:13 +0300

    Implement login functionality with event listener

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,37 @@
-await signInWithEmailAndPassword(
-    auth,
-    email,
-    password
-);
+loginBtn.addEventListener("click", async () => {
 
-alert("Giriş başarılı 🚗⚡");
+    alert("GİRİŞ BUTONU ÇALIŞTI");
 
-window.location.reload();
+    const email =
+    document.getElementById("email").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    console.log(email);
+    console.log(password);
+
+    try {
+
+        const userCredential =
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        console.log(userCredential);
+
+        alert("Giriş başarılı 🚗⚡");
+
+    } catch(error){
+
+        console.error(error);
+
+        alert(
+            error.code + "\n" + error.message
+        );
+
+    }
+
+});
