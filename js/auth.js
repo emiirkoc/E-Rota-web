12615f6ac7c81fd8b0615bb9eae8272c2d1c466a
emiirkoc <kocemir6634@gmail.com>
2026-06-07 02:16:17 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,77 @@
+import { auth } from "./firebase.js";
 
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginBtn =
+document.getElementById("loginBtn");
+
+const registerBtn =
+document.getElementById("registerBtn");
+
+registerBtn.addEventListener(
+"click",
+async () => {
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+if(email === "" || password === ""){
+alert("E-posta ve şifre giriniz");
+return;
+}
+
+try {
+
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+alert("Kayıt başarılı 🎉");
+
+}
+catch(error){
+
+alert(error.message);
+
+}
+
+}
+);
+
+loginBtn.addEventListener(
+"click",
+async () => {
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+try {
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+alert("Giriş başarılı 🚗⚡");
+
+}
+catch(error){
+
+alert(error.message);
+
+}
+
+}
+);
