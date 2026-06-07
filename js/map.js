cd68514f9d9d9ab7f829ca2714f627f994c1bb1f
emiirkoc <kocemir6634@gmail.com>
2026-06-07 18:11:59 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,43 +1,54 @@
 console.log("MAP BAŞLADI");
 
 const stations = [
+
 {
 name:"Trugo",
 lat:41.0082,
 lng:28.9784,
-doluluk:80,
+distance:"0.8 km",
+doluluk:"%80",
 price:"8.99 ₺/kWh"
 },
+
 {
 name:"ZES",
 lat:41.03,
 lng:29.05,
-doluluk:45,
+distance:"2.1 km",
+doluluk:"%45",
 price:"8.75 ₺/kWh"
 },
+
 {
 name:"Eşarj",
 lat:41.06,
 lng:29.02,
-doluluk:60,
+distance:"3.4 km",
+doluluk:"%60",
 price:"8.90 ₺/kWh"
 },
+
 {
 name:"Voltrun",
 lat:41.00,
 lng:29.12,
-doluluk:15,
+distance:"4.7 km",
+doluluk:"%15",
 price:"8.60 ₺/kWh"
 }
+
 ];
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", () => {
 
-const mapDiv=document.getElementById("map");
+const mapDiv =
+document.getElementById("map");
 
 if(!mapDiv) return;
 
-const map=L.map("map").setView(
+const map =
+L.map("map").setView(
 [41.0082,28.9784],
 11
 );
@@ -49,83 +60,50 @@ attribution:"© OpenStreetMap"
 }
 ).addTo(map);
 
-stations.forEach(station=>{
+stations.forEach(station => {
 
-let color="green";
-
-if(station.doluluk>70){
-color="red";
-}
-else if(station.doluluk>40){
-color="orange";
-}
-
-const marker=L.marker([
+L.marker([
 station.lat,
 station.lng
-]).addTo(map);
+])
 
-marker.bindPopup(`
+.addTo(map)
+
+.bindPopup(`
 <b>${station.name}</b>
 <br>
-⚡ Doluluk: %${station.doluluk}
+📍 ${station.distance}
+<br>
+⚡ Doluluk ${station.doluluk}
 <br>
 💰 ${station.price}
 `);
 
 });
 
-if(navigator.geolocation){
-
-navigator.geolocation.getCurrentPosition(
-
-(position)=>{
-
-const lat=position.coords.latitude;
-const lng=position.coords.longitude;
-
-L.marker([lat,lng])
-.addTo(map)
-.bindPopup("📍 Konumunuz")
-.openPopup();
-
-map.setView([lat,lng],13);
-
-},
-
-(error)=>{
-console.log(error);
-}
-
-);
-
-}
-
-loadStations();
-
-});
-
-function loadStations(){
-
-const container=
+const list =
 document.getElementById("stationsList");
 
-if(!container) return;
+if(list){
 
-container.innerHTML="";
+stations.forEach(station => {
 
-stations.forEach(station=>{
-
-container.innerHTML+=`
+list.innerHTML += `
 <div class="station-card">
-<h3>${station.name}</h3>
 
-<p>⚡ Doluluk: %${station.doluluk}</p>
+<h3>⚡ ${station.name}</h3>
+
+<p>📍 ${station.distance}</p>
+
+<p>⚡ Doluluk ${station.doluluk}</p>
 
 <p>💰 ${station.price}</p>
+
 </div>
 `;
 
 });
 
 }
+
+});
