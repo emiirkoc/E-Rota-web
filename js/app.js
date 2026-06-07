05ffb5ab660febf121e9b893a99c8bdee6e4af4c
emiirkoc <kocemir6634@gmail.com>
2026-06-07 17:16:46 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
-alert("APP ÇALIŞTI");
-console.log("APP ÇALIŞTI");
+```javascript
+function showPage(pageId){
+
+document.getElementById("homePage").style.display="none";
+document.getElementById("mapPage").style.display="none";
+document.getElementById("stationsPage").style.display="none";
+document.getElementById("profilePage").style.display="none";
+
+document.getElementById(pageId).style.display="block";
+
+if(pageId==="mapPage"){
+setTimeout(()=>{
+window.dispatchEvent(new Event("resize"));
+},200);
+}
+
+}
+```
