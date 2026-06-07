320bac13e659fc53d99a1a0075f320edbb190289
emiirkoc <kocemir6634@gmail.com>
2026-06-07 19:38:26 +0300

    Add charging station data to stations.js

--- js/stations.js
+++ js/stations.js
@@ -1,1 +1,53 @@
+window.stations = [
 
+{
+id:1,
+name:"ADZE Charge",
+lat:41.055,
+lng:29.021,
+occupancy:25,
+distance:"0.5 km",
+price:"8.50 ₺/kWh"
+},
+
+{
+id:2,
+name:"Trugo",
+lat:41.0082,
+lng:28.9784,
+occupancy:80,
+distance:"0.8 km",
+price:"8.99 ₺/kWh"
+},
+
+{
+id:3,
+name:"ZES",
+lat:41.03,
+lng:29.05,
+occupancy:45,
+distance:"2.1 km",
+price:"8.75 ₺/kWh"
+},
+
+{
+id:4,
+name:"Eşarj",
+lat:41.06,
+lng:29.02,
+occupancy:60,
+distance:"3.4 km",
+price:"8.90 ₺/kWh"
+},
+
+{
+id:5,
+name:"Voltrun",
+lat:41.00,
+lng:29.12,
+occupancy:15,
+distance:"4.7 km",
+price:"8.60 ₺/kWh"
+}
+
+];
